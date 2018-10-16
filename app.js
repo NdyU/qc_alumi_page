cf6789848f8275f921ee61a4c0073f9f8fd9deec
Shinan Liu <shinan@Shinans-MacBook-Pro.local>
2018-10-15 22:16:51 -0400

    testing branch

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 //router.js define all routes in this server
 const router = require('./router');
 // Passing app to routers, using router.js to define all the routes
