c1f77ccf40a1230003a11dae1129404c9875643b
linyoujie <linyoujie@hotmail.com>
2018-11-12 15:19:29 -0500

    Web UIUX
    set BG Picture for Register Login and About Us.

--- router.js
+++ router.js
@@ -9,4 +9,28 @@ module.exports = function(app) {
   app.get('/home', function(req, res) {
     res.render('sample');
   })
+  
+    app.get('/aboutUs', function(req, res) {
+    res.render('aboutUs');
+  })
+  
+    app.get('/donateUs', function(req, res) {
+    res.render('donateUs');
+  })
+  
+    app.get('/contactUs', function(req, res) {
+    res.render('contactUs');
+  })
+  
+    app.get('/ourTeam', function(req, res) {
+    res.render('ourTeam');
+  })
+  
+    app.get('/login', function(req, res) {
+    res.render('login');
+  })
+  
+    app.get('/register', function(req, res) {
+    res.render('register');
+  })
 }
